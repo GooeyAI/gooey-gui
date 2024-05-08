21c46038bc6194ced321136ade8e9b8c01cedc5d
clr-li <111320104+clr-li@users.noreply.github.com>
2024-05-07 22:15:38 -0700

    Smaller number input

--- app/base.tsx
+++ app/base.tsx
@@ -275,7 +275,7 @@ function RenderedTreeNode({
       return <GooeyTextarea props={props} state={state} />;
     }
     case "input": {
-      const className = `gui-input gui-input-${props.type}`;
+      const className = `gui-input gui-input-${props.type} ${props.className ?? ""}`;
       const id = inputId(props);
       switch (props.type) {
         case "range":
