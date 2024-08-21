8bc9c880df4c51028697efb8ce8a5afc583567d9
anish-work <anish.saxenaa@gmail.com>
2024-08-21 15:56:00 +0530

    remove js for tooltip

--- app/renderedMarkdown.tsx
+++ app/renderedMarkdown.tsx
@@ -1,64 +1,28 @@
 import { marked } from "marked";
-import { useRef } from "react";
 import { RenderedHTML } from "~/renderedHTML";
-
-const getDialog = (event: React.MouseEvent) => {
-  return event.currentTarget.tagName === "DIALOG"
-    ? event.currentTarget.closest("dialog")
-    : (event.currentTarget.nextElementSibling as HTMLDialogElement);
-};
 
 export function GooeyTooltip(props: {
   text: string;
   direction?: "left";
   iconStyle?: Record<string, string>;
 }) {
-  const timerRef = useRef<NodeJS.Timeout | null>(null);
-  const showTooltip = (event: React.MouseEvent) => {
-    if (timerRef.current) {
-      clearTimeout(timerRef.current);
-      timerRef.current = null;
-    }
-
-    const dialog = getDialog(event);
-    dialog!.show();
-    event.preventDefault();
-  };
-
-  const hideTooltip = (event: React.MouseEvent) => {
-    const dialog =getDialog(event);
-    timerRef.current = setTimeout(() => dialog!.close(), 200);
-    event.preventDefault();
-  };
-
   return (
     <span
       className={
         "gui_tooltip" +
-        (props.direction ? " gui_tooltip--" + props.direction : "")
+        (props.direction
+          ? " gui_tooltip--" + props.direction
+          : " gui_tooltip--right")
       }
     >
       <i
         role="button"
-        onMouseEnter={showTooltip}
-        onMouseLeave={hideTooltip}
         className="fa-regular fa-circle-info"
-        style={props.iconStyle}
+        style={{ height: "16px", ...props.iconStyle }}
       ></i>
-      <dialog onMouseEnter={showTooltip} onMouseLeave={hideTooltip}>
-        <input
-          style={{
-            width: "0",
-            height: "0",
-            minWidth: "0",
-            border: "0",
-            outline: "0",
-            padding: "0",
-            margin: "0",
-          }}
-        ></input>
+      <span className="tooltip_text">
         <RenderedMarkdown body={props.text} className="" />
-      </dialog>
+      </span>
     </span>
   );
 }
