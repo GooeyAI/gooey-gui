cdd7174adb228eeefaaa9810a3901543608ca109
Dev Aggarwal <devxpy@gmail.com>
2024-09-02 13:52:33 +0530

    reduce sentry usage

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -13,7 +13,7 @@ Sentry.init({
       routingInstrumentation: Sentry.remixRouterInstrumentation(
         useEffect,
         useLocation,
-        useMatches,
+        useMatches
       ),
     }),
     new Sentry.Replay(),
@@ -21,9 +21,9 @@ Sentry.init({
   ],
   release: window.ENV.SENTRY_RELEASE,
   // Performance Monitoring
-  tracesSampleRate: parseFloat(window.ENV.SENTRY_SAMPLE_RATE ?? "0.1"), // Capture X% of the transactions, reduce in production!
+  tracesSampleRate: 0.005, // Capture X% of the transactions, reduce in production!
   // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysSessionSampleRate: 0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
   // This option is required for capturing headers and cookies.
   sendDefaultPii: true,
--- app/settings.ts
+++ app/settings.ts
@@ -2,6 +2,5 @@ export default {
   SERVER_HOST: process.env.SERVER_HOST || "http://127.0.0.1:8080",
   REDIS_URL: process.env.REDIS_URL,
   SENTRY_DSN: process.env.SENTRY_DSN,
-  SENTRY_SAMPLE_RATE: parseFloat(process.env.SENTRY_SAMPLE_RATE ?? "0.1"),
   SENTRY_RELEASE: process.env.SENTRY_RELEASE,
 };
